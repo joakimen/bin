38814996c3bb95983533324ba12e7cc7de0419dd
Joakim L. Engeset <joakim.engeset@gmail.com>
2022-12-06 12:41:57 +0100

    Cleanup and simplify

--- awschrome.sh
+++ awschrome.sh
@@ -4,32 +4,24 @@
 
 profile="$1"
 if [[ -z "$profile" ]]; then
-    echo "Profile is a required argument" >&2
+    echo "usage: $0 <aws-profile>" >&2
     return 1
 fi
 
-# replace non word and not - with __
-profile_dir_name=${profile//[^a-zA-Z0-9_-]/__}
-browser_data=$(mktemp -d /tmp/awschrome_userdata.XXXXXXXX)
-new_window_arg=''
-
 # run aws-vault
-# --prompt osascript only works on OSX
-url=$(aws-vault login $profile --stdout --prompt osascript)
+url=$(aws-vault login "$profile" --stdout --prompt osascript)
 status=$?
 
-if [[ ${status} -ne 0 ]]; then
+if [[ $status -ne 0 ]]; then
     # bash will also capture stderr, so echo $url
-    echo ${url}
-    return ${status}
+    echo "$url"
+    return $status
 fi
 
-mkdir -p ${user_data_dir}
-browser_cache=$(mktemp -d /tmp/awschrome_cache.XXXXXXXX)
+# mkdir -p "$user_data_dir"
 /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
     --no-first-run \
-    --user-data-dir=${browser_data} \
-    --disk-cache-dir=${browser_cache} \
-    ${new_window_arg} \
-    ${url} \
-  >/dev/null 2>&1 &
+    --user-data-dir="$(mktemp -d /tmp/awschrome_userdata.XXXXXXXX)" \
+    --disk-cache-dir="$(mktemp -d /tmp/awschrome_cache.XXXXXXXX)" \
+    "$url" \
+    >/dev/null 2>&1 &
