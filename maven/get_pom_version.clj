ec08ecd9c718d086dc3091a60f93d52cd90ef770
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-06 07:04:24 +0100

    Rename get-pom-version for bbin support

--- maven/get_pom_version.clj
+++ maven/get_pom_version.clj
@@ -1,7 +1,8 @@
 #!/usr/bin/env bjb
 (ns maven.get-pom-version
   (:require [clojure.data.xml :as xml]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [babashka.fs :as fs]))
 
 (defn tag-name? [tag tname]
   (some-> tag :tag name #{tname}))
@@ -18,4 +19,7 @@
        tag-content-str))
 
 (let [pom (or (first *command-line-args*) "pom.xml")]
+  (when (not (fs/exists? pom))
+    (println (str pom ": No such file or directory"))
+    (System/exit 1))
   (-> pom get-version println))
