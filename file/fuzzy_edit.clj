195fb5bb10392e857c9719a4fb671693af15b355
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-10 20:46:05 +0100

    Finish fuzzy-edit.clj

---
+++ file/fuzzy_edit.clj
@@ -0,0 +1,35 @@
+#!/usr/bin/env bb
+(ns file.fuzzy-edit
+  (:require [babashka.fs :as fs]
+            [babashka.process :as p]
+            [clojure.string :as str]))
+
+(let [bin "fzf"]
+  (when (not (fs/which bin))
+    (println "missing required binary:" bin)
+    (System/exit 1)))
+
+(def preview-commands
+  [{:pri 1 :bin "bat" :cmd "bat --line-range :20 --number --color=always {}"}
+   {:pri 2 :bin "cat" :cmd "cat {}"}])
+
+(defn fzf []
+  (let [preview-cmd (->> preview-commands
+                         (filter #(fs/which (:bin %)))
+                         (sort-by :pri) first :cmd)
+        proc @(p/process ["fzf" "-m" "--preview"
+                          preview-cmd "--preview-window" "70%"]
+                         {:in :inherit
+                          :out :string
+                          :err :inherit})]
+    (when (> (:exit proc) 0)
+      (System/exit 0))
+    (-> proc :out str/trim str/split-lines)))
+
+(apply p/shell "nvim" (fzf))
+
+(comment
+  (defn debug [s]
+    (doto s (println)))
+  ;
+  )
