b2512600829ca13c9f680b975fb132c7b45e88c7
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-10 12:26:26 +0100

    Add glue-script to open N aws-profiles on chrome

---
+++ aws/aws_vault/open_aws_chome.clj
@@ -0,0 +1,15 @@
+(ns aws.aws-vault.open-aws-chome
+  "select N aws profiles, then open them in aws_chrome"
+  (:require [babashka.process :as p]
+            [clojure.string :as str]))
+
+(defn select-aws-profiles []
+  (let [res (p/sh {:err :inherit} "select-aws-profiles")]
+    (when (> (:exit res) 0)
+      (System/exit 1)) ;; probs just caught ctrl-c
+    (->> res :out str/trim str/split-lines)))
+
+(defn open-in-awschrome [profile]
+  (p/shell "aws-chrome" profile))
+
+(pmap open-in-awschrome (select-aws-profiles))
