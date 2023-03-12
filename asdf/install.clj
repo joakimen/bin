d3ab83a3e5d028ab71050ab5511d6a9009908158
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-12 01:56:38 +0100

    Cleanup arg usage

--- asdf/install.clj
+++ asdf/install.clj
@@ -5,19 +5,19 @@
 
 ;; install asdf-plugins, filtered through fzf
 
-(defn run [cmd]
-  (let [res (p/sh cmd)]
+(defn run [& args]
+  (let [res (apply p/sh args)]
     (when (> (:exit res) 0)
       (->> res :err str/trim println)
       (System/exit (:exit res)))
     (->> res :out str/trim)))
 
 (defn list-languages []
-  (->> (run "asdf plugin list")
+  (->> (run "asdf" "plugin" "list")
        (str/split-lines)))
 
 (defn list-versions [lang]
-  (->> (run (str "asdf list all " lang))
+  (->> (run "asdf" "list" "all" lang)
        (str/split-lines)
        (assoc {} :lang lang :versions)))
 
