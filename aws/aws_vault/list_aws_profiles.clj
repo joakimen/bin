b2803a1b8797e0cb13966aa1885ef0556fa17788
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-13 10:11:51 +0100

    Speed up list-aws-profiles

--- aws/aws_vault/list_aws_profiles.clj
+++ aws/aws_vault/list_aws_profiles.clj
@@ -1,4 +1,12 @@
 (ns aws.aws-vault.list-aws-profiles
-  (:require [babashka.process :as p]))
+  (:require [babashka.fs :as fs]))
 
-(p/shell "aws" "configure" "list-profiles")
+(let [conf (fs/read-all-lines (str (fs/path (fs/home) ".aws" "config")))
+      pat #"\[profile ([a-zA-Z0-9\-]+)"]
+
+  (->> conf
+       (filter #(re-find pat %))
+       (map #(second (re-find pat %)))
+       (mapv println)
+ ;; there's probably a less knuckle-headed way of doing this.. 
+       ))
