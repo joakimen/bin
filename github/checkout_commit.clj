9b761f9553eaf79a2d14dbe2a461c489db62a863
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-16 11:19:12 +0100

    Add script: checkout a given revision using fzf

---
+++ github/checkout_commit.clj
@@ -0,0 +1,11 @@
+(ns github.checkout-commit
+  "checkout a given revision using fzf"
+  (:require [babashka.process :as p]
+            [clojure.string :as str]))
+
+(let [res (p/sh {:err :inherit :out :string} "select-commit")]
+  (when (> (:exit res) 0)
+    (System/exit (:exit res)))
+  (let [revision (-> res :out str/trim)]
+    (println "checking out revision:" revision)
+    (p/shell "git" "checkout" revision)))
