63dcf11df53829e9fd0cfe859b65be0568fa91ce
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-18 13:22:10 +0100

    Clarify docstring

--- file/list_filedirs.clj
+++ file/list_filedirs.clj
@@ -1,6 +1,6 @@
 #!/usr/bin/env bb
 (ns file.list-filedirs
-  "list all files recursively from cwd, filter with fzf. return parent-dir of selected file"
+  "return parent-dir of selected file"
   (:require [babashka.fs :as fs]
             [babashka.process :as p]
             [clojure.string :as str]))
