c721a609abd4fcfdfacc45ffc668c4f4572604a7
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-20 10:47:29 +0100

    obsidian/review wip

--- obsidian/review.clj
+++ obsidian/review.clj
@@ -1,23 +1,39 @@
 (ns obsidian.review
   {:clj-kondo/ignore true} ;;calva isn't updated yet, but clj-kondo now ignores this
   "do a health-check on a local obsidian-vault and report problems
-   - files named Unnamed - empty or less than 2 lines")
+   - files named Unnamed - empty or less than 2 lines"
+  (:require [babashka.fs :as fs]
+            [clj-yaml.core :as clj-yaml]))
 
-(def config-file "TODO")
+(def config-file (str (fs/path (fs/xdg-config-home) "projects.yaml")))
 
 (defn read-config []
-  ;; TODO: read config-file to string
-  )
+  (->> config-file
+       slurp
+       clj-yaml/parse-string))
 
-(defn obsidian-review/parse-yaml [contents]
-  ;; TODO parse string to yaml
-  )
 
-(defn parse-config [config-yaml]
+(defn parse-config [config]
+  (let [notes-config (:notes config)
+        existing-dirs (->> notes-config
+                           :folders
+                           (filter #(fs/directory? (fs/path (fs/expand-home %))))
+                           (map str))]
+    (println "existing dirs:"  existing-dirs)
+    ;; ( (:folders notes-config)
+    ;;      (map #(println "folder:" %)
+    ;
+    )
+  ;
+  )
+  ;; notes
+  ;;   root
+  ;;   rules [list]
   ;; parse yaml, handle nulls, defaults, etc
   ;; - vaults [vector of paths]
   ;; - rules [no_untitled, no_empty]
-  )
+
+(parse-config (read-config))
 
 (defn get-vault [vault]
   ;; parse relpath to abspath
