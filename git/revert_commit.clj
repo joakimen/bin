17ec9adb01cf2178cd24a1dda1aaf8b59d0f2bca
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-22 10:16:37 +0100

    Add error handling

--- git/revert_commit.clj
+++ git/revert_commit.clj
@@ -2,7 +2,14 @@
   (:require [babashka.process :as p]
             [clojure.string :as str]))
 
-(let [res (p/sh {:err :inherit :out :string} "select-commit")
-      sha (-> res :out str/trim)]
-  (println "reverting revision:" sha)
-  (p/shell {:continue true} "git" "revert" sha))
+(let [{:keys [out exit]} (p/sh {:err :inherit :out :string} "select-commit")]
+
+  (when-not (zero? exit)
+    (System/exit 1)) ;; just quiet ctrl-c
+
+  (when-not (string? out)
+    (throw (ex-info (str "expected sha: " out) {:babashka/exit 1})))
+
+  (let [sha (str/trim out)]
+    (println "reverting revision:" sha)
+    (p/shell {:continue true} "git" "revert" sha)))
