8e50c7b1147672eaa2ee55e4ddee2a2cef49388d
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-23 23:13:00 +0100

    Simplify revert-commit

--- git/revert_commit.clj
+++ git/revert_commit.clj
@@ -1,15 +1,11 @@
 (ns git.revert-commit
-  (:require [babashka.process :as p]
-            [clojure.string :as str]))
+  (:require [babashka.process :as p]))
 
-(let [{:keys [out exit]} (p/sh {:err :inherit :out :string} "select-commit")]
-
+(let [{:keys [out exit]}
+      (-> (p/process "list-commits")
+          (p/process ["fzf"] {:err :inherit
+                              :out :string})
+          deref)]
   (when-not (zero? exit)
-    (System/exit 1)) ;; just quiet ctrl-c
-
-  (when-not (string? out)
-    (throw (ex-info (str "expected sha: " out) {:babashka/exit 1})))
-
-  (let [sha (str/trim out)]
-    (println "reverting revision:" sha)
-    (p/shell {:continue true} "git" "revert" sha)))
+    (System/exit exit))
+  (p/shell {:continue true} "git" "revert" (re-find #"^[a-f0-9]+" out)))
