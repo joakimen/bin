a9c1dcce52a34f24838f245e35d570313ab04d41
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-03-25 20:22:24 +0100

    Some improvements

--- git/pull.clj
+++ git/pull.clj
@@ -1,113 +1,59 @@
 #!/usr/bin/env bb
 (ns git.pull
-  "
-   WIP
-
-   pulls updates to the given list of repositories
- 
-   Idea: Incorporate pull-flag (bool) in projects.yaml?
- 
-   # requirements
- 
-   determine projects (hard-coded list is not feasible)
-   - dev-root from stdin
-     - alt 1) dev-root argument (e.g. ~/dev) + use hard-coded recursion depth
-     - alt 2) dev-root env var (e.g. ~/dev) + use hard-coded recursion depth
-   - configuration file
-     - alt 1) dir (+ depth?)
-       - 1 level sounds fair
-     - which format? json/yaml/toml/text
-       - probably yaml for this
- 
-   choosing
-   - yaml configuration file
-   - entries = dirnames
-   - 1 level depth, e.g.
-     - dirname: /dev/github
-     - resolves
-       - /dev/github/proj-a
-       - /dev/github/proj-b
- 
-   pull-operation
-   - rebase-pull N repos by abspath
-   - async & gather results (pmap / async channels?)
-   "
+  "WIP - ebase-pull all tracked repos. dirty repos are skipped"
   (:require [babashka.process :refer [sh]]
-            [clj-yaml.core :as clj-yaml]
-            [clojure.string :as str]
-            [babashka.fs :as fs]
-            [taoensso.timbre :as log]))
+            [clojure.pprint :as pprint]
+            [clojure.string :as str]))
 
-(def config-file (str (System/getenv "HOME") "/.config/pullconf.yaml"))
+(defn run [& args]
+  (let [{:keys [out err exit]} (apply sh args)]
+    (when-not (zero? exit)
+      (throw (ex-info (str/trim err) {:babashka/exit exit})))
+    (str/trim out)))
 
-(defn read-config []
-  (slurp config-file))
-
-(defn parse-yaml [s]
-  (clj-yaml/parse-string s))
+(defn list-projects []
+  (str/split-lines (run "list-projects")))
 
 (defn zero-exit? [dir & args]
-  (let [exit-code (:exit (apply sh {:dir dir} args))]
-    (= exit-code 0)))
+  (zero? (:exit (apply sh {:dir dir} args))))
 
-(defn repo? [repo-path]
-  (zero-exit? repo-path "git rev-parse @"))
-
-(defn clean? [repo-path]
+(defn is-clean? [repo-path]
   (zero-exit? repo-path "git diff --quiet"))
 
 (defn get-current-branch [repo]
-  (-> (sh {:dir repo} "git branch --show-current")
-      :out str/trim-newline))
+  (run "git" "-C" repo "branch" "--show-current"))
 
 (defn- pull-repo [repo]
-  ;; just running git status for now, needs more testing
-
-  (let [branch (get-current-branch repo)
-        cmd (format "git -C %s status --short" repo)
-        res (sh {:dir repo} cmd)]
-
+  (let [branch (get-current-branch repo)]
     (println "pulling repo:" repo "branch:" branch)
-    {:repo repo
-     :exit (:exit res)
-     :out (:out res)
-     :err (:err res)})
+    (assoc (select-keys (sh "git" "-C" repo "pull" "--rebase" "--autostash" "origin" branch) [:out :err :exit]) :repo repo)))
 
-  ;; (let [branch (get-current-branch repo)
-  ;;       cmd (format "git pull --rebase --autostash origin %s" branch)
-  ;;       res (sh {:dir repo} cmd)]
-  ;;   {:repo repo
-  ;;    :exit (:exit res)
-  ;;    :out (:out res)
-  ;;    :err (:err res)})
- ;; 
-  )
+(defn parse-repo-shortname [s]
+  (let [pat #".*/([a-zA-Z0-9]+/[a-zA-Z0-9-]+)$"
+        res (second (re-find pat s))]
+    (when (nil? res)
+      (throw (ex-info (str "failed to parse repo shortname from: " s) {:babashka/exit 1})))
+    res))
 
-;; 1. read config file
-(let [dir-entries (->> (read-config)
-                       (parse-yaml)
-                       :dirs
-                       (map fs/expand-home))]
+(->> (list-projects)
+     (filter is-clean?)
+     (pmap pull-repo)
+     (map #(update % :repo parse-repo-shortname)) ;; shorten name for printing
+     (map #(dissoc % :out :err)) ;; until we have a better way to print this
+     (doall)
+     (pprint/print-table))
 
- ;; warn for non-existent dir entries in 
-  (->> dir-entries
-       (filter #(not (fs/directory? %)))
-       (mapv #(log/warn (str "no such dir: " % ", skipping"))))
+(comment
 
+  (def p (list-projects))
 
-  ;; git-pull the existing ones
-  (->> dir-entries
-       (filter fs/directory?)
-       (pmap fs/list-dir)
-       (flatten)
-       (filter #(not (fs/hidden? %)))
-       (map str)
-       (filter repo?)
-       (filter clean?)
+  (->> p
+       (filter is-clean?)
        (pmap pull-repo)
-       (mapv println)
+       (map #(update % :repo parse-repo-shortname))
+       (map #(dissoc % :out :err))
+       (doall)
+       (pprint/print-table))
 
-;;
-       )
  ;; 
   )
