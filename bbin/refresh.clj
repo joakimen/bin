eb81d49104e5134eb391d64ab1f3024b44296f3d
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-04-05 14:11:01 +0200

    Hard-code script path to workaround bbin cwd

--- bbin/refresh.clj
+++ bbin/refresh.clj
@@ -1,16 +1,16 @@
 #!/usr/bin/env bb
 (ns bbin.refresh
+  "desired packages, until bbin supports a declarative package config"
   (:require [babashka.fs :as fs]
             [babashka.process :as p]))
-
-;; desired packages, until bbin supports a declarative package config
 
 (defn in? [coll e]
   (some #(= e %) coll))
 
-(let [ignores ["obsidian/review.clj"]
+(let [ignores ["obsidian/review.clj"
+               "bbin/refresh.clj"]
       remotes ["https://raw.githubusercontent.com/borkdude/tools/main/cljfmt.clj"]
-      locals (->> (fs/glob (-> *file* fs/parent fs/parent) "**.clj")
+      locals (->> (fs/glob (fs/path (fs/home) "bin") "**.clj")
                   (map str)
                   (filter #(not (in? ignores %))))
       to_install (distinct (into remotes locals))]
