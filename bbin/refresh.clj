07647ff2e39e884c364a6ee10aa3b3007885a0f3
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-04-06 16:19:41 +0200

    add listing of installed scripts

--- bbin/refresh.clj
+++ bbin/refresh.clj
@@ -2,11 +2,13 @@
 (ns bbin.refresh
   "desired packages, until bbin supports a declarative package config"
   (:require [babashka.fs :as fs]
-            [babashka.process :as p]))
+            [babashka.process :as p]
+            [edamame.core :as eda]))
 
 (defn in? [coll e]
   (some #(= e %) coll))
 
+;; install scripts
 (let [ignores ["obsidian/review.clj"
                "bbin/refresh.clj"]
       remotes ["https://raw.githubusercontent.com/borkdude/tools/main/cljfmt.clj"]
@@ -14,4 +16,12 @@
                   (map str)
                   (filter #(not (in? ignores %))))
       to_install (distinct (into remotes locals))]
-  (mapv #(p/shell "bbin" "install" %) to_install))
+  (doall (pmap #(p/shell "bbin" "install" %) to_install)))
+
+;; list installations
+(let [res (:out (p/sh {:out :string} "bbin" "ls"))
+      parsed (eda/parse-string res)]
+  (->> parsed
+       (map #(assoc {} :script (key %) :coords (-> (val %) :coords :bbin/url)))
+       (sort-by :script)
+       (mapv #(println (:script %) (:coords %)))))
