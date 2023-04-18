c575ea65cfa0336baf2e3a44c8fe269669a006fd
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-04-18 22:55:47 +0200

    Add first draft of review.clj
    - Determine vaults and files for notes
    - Find notes with problems and add it to a log
    
    Later on, parse the log and do something with it (delete untitledm, etc)

--- obsidian/review.clj
+++ obsidian/review.clj
@@ -1,56 +1,92 @@
 (ns obsidian.review
-  {:clj-kondo/ignore true} ;;calva isn't updated yet, but clj-kondo now ignores this
   "do a health-check on a local obsidian-vault and report problems
-   - files named Unnamed - empty or less than 2 lines"
-  (:require [babashka.fs :as fs]
-            [clj-yaml.core :as clj-yaml]))
+   - files named Unnamed 
+   - empty or less than 2 lines
+   - no links/backlinks (can we do this?)
+   
+   1. Determine vaults
+   2. For each vault, run checks
+   3. Log problems with each file
+   4. Parse log and take necessary action (delete files, print, ..)
+   "
 
-(def config-file (str (fs/path (fs/xdg-config-home) "projects.yaml")))
+  (:require [babashka.fs :as fs]
+            [clojure.edn :as edn]))
+
+(def config-file (fs/file (fs/xdg-config-home) "notes.edn"))
 
 (defn read-config []
-  (->> config-file
-       slurp
-       clj-yaml/parse-string))
+  (-> config-file slurp edn/read-string))
 
+(defn is-vault? [dir]
+  (fs/directory? (fs/path dir ".obsidian")))
 
-(defn parse-config [config]
-  (let [notes-config (:notes config)
-        existing-dirs (->> notes-config
-                           :folders
-                           (filter #(fs/directory? (fs/path (fs/expand-home %))))
-                           (map str))]
-    (println "existing dirs:"  existing-dirs)
-    ;; ( (:folders notes-config)
-    ;;      (map #(println "folder:" %)
-    ;
-    )
-  ;
-  )
-  ;; notes
-  ;;   root
-  ;;   rules [list]
-  ;; parse yaml, handle nulls, defaults, etc
-  ;; - vaults [vector of paths]
-  ;; - rules [no_untitled, no_empty]
+(defn list-subdirs [dir]
+  (->> (fs/list-dir dir)
+       (filter #(fs/directory? %))))
 
-(parse-config (read-config))
+(defn is-empty-ish?
+  "- if file has 0 lines, return true
+   - if file has 1 line, and the line is a md h1-heading, return true
+   else, return false"
+  [path]
+  (let [lines (fs/read-all-lines path)]
+    (if (or (zero? (count lines))
+            (and (= (count lines) 1)
+                 (re-matches #"^#\s.*" (first lines))))
+      true false)))
 
-(defn get-vault [vault]
-  ;; parse relpath to abspath
-  ;; ensure dir exists and is not empty
-  )
+(defn is-untitled?
+  "if file is named Untitled-something, return true
+   else, return false"
+  [path]
+  (if (re-matches #"^Untitled(\s\d+)?\.md" (fs/file-name path))
+    true false))
 
-(defn list-vault-files [vault]
-  ;; return vector of filenames in vault
-  ;; - prereq: what is the structure of an obsidian vault? what to ignore?
-  )
+(defn check-vault
+  "performs all actions (checks, cleanup, ..) for a given vault"
+  [vault rule-fns]
+  (->> (pmap #(% vault) rule-fns)
+       (doall)))
 
-(defn isUntitledFile? [filepath]
-  ;; return true if file is named "Untitled.*"
-  ;; else false
-  )
+(def operation-log (atom {}))
 
-(defn isEmpty? [filepath]
-  ;; return true if file is empty or has less than 1 line (the title)
-  ;; else false
+(defn log-operation! [problem file]
+  (swap! operation-log update-in [file] conj problem))
+
+(defn no-untitled
+  "remove markdown-files whose name starts Untitled"
+  [vault]
+  (println "checking for untitled files in vault:" vault)
+  (->> (fs/glob vault "**.md")
+       (map str)
+       (filter is-untitled?)
+       (mapv #(log-operation! :untitled %))))
+
+(defn no-empty
+  "list files that are empty or have less than 2 lines"
+  [vault]
+  (println "checking for empty files in vault:" vault)
+  (->> (fs/glob vault "**.md")
+       (map str)
+       (filter is-empty-ish?)
+       (mapv #(log-operation! :empty %))))
+
+(let [config (read-config)
+      rule-fns (map #(% {:no_untitled no-untitled
+                         :no_empty no-empty}) (:rules config))
+      notes-path (fs/expand-home (:path config))
+      vault-list (->> (list-subdirs notes-path)
+                      (filter is-vault?))]
+  (doall (pmap #(check-vault % rule-fns) vault-list))
+
+  (->> @operation-log
+       sort
+       (mapv println)))
+
+(comment
+
+  (reset! operation-log {})
+
+  ;;
   )
