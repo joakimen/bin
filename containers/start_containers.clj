063b6a1bd86344dd35b531e3d0e045399e34d707
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-05-19 15:32:01 +0200

    rewrite start-containers to clj

---
+++ containers/start_containers.clj
@@ -0,0 +1,29 @@
+(ns containers.start-containers
+  (:require [babashka.process :as p]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(defn run [& args]
+  (let [{:keys [out err exit]} (apply p/sh args)]
+    (if (zero? exit)
+      (str/trim out)
+      (throw (ex-info (str/trim err) {:babashka/exit exit})))))
+
+
+(defn fzfm [v]
+  (let [{:keys [out exit]} @(p/process ["fzf" "-m"]
+                                       {:in (str/join "\n" v) :err :inherit
+                                        :out :string})]
+    (if (zero? exit)
+      (-> out str/trim str/split-lines)
+      (System/exit exit))))
+
+
+(defn list-startable-containers []
+  (edn/read-string (run "list-startable-containers")))
+
+(->> (list-startable-containers)
+     (mapv :Names)
+     (fzfm)
+     (pmap #(p/shell "docker" "start" %))
+     (doall))
