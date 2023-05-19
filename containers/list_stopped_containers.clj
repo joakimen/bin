584053a85f2008a5ebb9fe22f26474dfec40d087
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-05-19 15:50:59 +0200

    fix ns

--- containers/list_stopped_containers.clj
+++ containers/list_stopped_containers.clj
@@ -1,4 +1,4 @@
-(ns containers.list-startable-containers
+(ns containers.list-stopped-containers
   (:require [babashka.process :as p]
             [clojure.edn :as edn]
             [clojure.string :as str]))
