8685fe37362b465ac349092d5157c9b3bedbba87
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-05-19 15:08:34 +0200

    add: containers/get_startable_containers.clj

---
+++ containers/get_startable_containers.clj
@@ -0,0 +1,21 @@
+(ns containers.get-startable-containers
+  (:require [babashka.process :as p]
+            [clojure.edn :as edn]
+            [clojure.string :as str]))
+
+(defn run [& args]
+  (let [{:keys [out err exit]} (apply p/sh args)]
+    (if (zero? exit)
+      (str/trim out)
+      (throw (ex-info (str/trim err) {:babashka/exit exit})))))
+
+(defn list-containers []
+  (edn/read-string (run "list-containers")))
+
+(defn container-startable? [container]
+  (some #(= (:State container) %) ["running" "exited"]))
+
+(->>
+ (list-containers)
+ (filter container-startable?)
+ (prn))
