7dea0ed5af25ece9dbd6185ed28584fe5079ecd4
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-05-30 15:47:00 +0200

    Use fzf from lib

--- bbgit/src/bbgit/core.clj
+++ bbgit/src/bbgit/core.clj
@@ -1,26 +1,27 @@
 (ns bbgit.core
   (:require [bbgit.lib.exception :refer [die]]
-            [bbgit.lib.fzf :as f]
             [bbgit.lib.git :as g]
-            [bbgit.lib.gum :as gum]))
+            [bbgit.lib.gum :as gum]
+            [fzf.core :refer [fzf]]))
 
 (defn switch-branch []
   (let [branches (g/list-branches)]
     (when (zero? (count branches))
       (die "no branches found"))
-    (-> branches f/fzf g/switch-branch)))
+    (-> (fzf {:in branches}) g/switch-branch)))
 
 (defn revert-commit []
   (let [commits (g/list-commits)
-        commit (->> commits f/fzf (re-find #"^[a-f0-9]+"))]
+        commit (->> (fzf {:in commits}))]
     (when-not commit
-      (die "couldn't parse selected commit"))
-    (println "Reverting commit" commit)
-    (g/revert-commit commit)))
+      (throw (ex-info nil {:babashka/exit 1})))
+    (let [commit (re-find #"^[a-f0-9]+" commit)]
+      (println "Reverting commit" commit)
+      (g/revert-commit commit))))
 
 (defn checkout-pull-request []
   (let [pull-requests (g/list-pull-requests)
-        pull-request (->> pull-requests f/fzf (re-find #"\d+"))]
+        pull-request (->> (fzf {:in pull-requests}) (re-find #"\d+"))]
     (when-not pull-request
       (die "couldn't parse selected pull request"))
     (println "Checking out pull request" pull-request)
@@ -28,7 +29,7 @@
 
 (defn checkout-commit []
   (let [commits (g/list-commits)
-        commit (->> commits f/fzf (re-find #"^[a-f0-9]+"))]
+        commit (->> (fzf {:in commits}) (re-find #"^[a-f0-9]+"))]
     (when-not commit
       (die "couldn't parse selected commit"))
     (println "Checking out commit" commit)
@@ -38,7 +39,9 @@
   (prn (g/list-branches)))
 
 (defn delete-branches []
-  (let [branches (-> (g/list-branches) (f/fzfm))]
+  (let [branches (g/list-branches)
+        to-delete (-> (fzf {:multi true
+                            :in branches}))]
     (when (empty? branches)
       (die "no branches selected"))
     (println "Will delete branches")
