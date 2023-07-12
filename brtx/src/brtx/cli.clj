e8e2860bb5e406922fa17a043af20e2a4eabe64a
Joakim Lindeng Engeset <joakim.engeset@gmail.com>
2023-07-12 17:46:43 +0200

    Handle nils in call chain

--- brtx/src/brtx/cli.clj
+++ brtx/src/brtx/cli.clj
@@ -1,7 +1,6 @@
 (ns brtx.cli
   (:require [babashka.process :as p]
             [cheshire.core :as json]
-            [clojure.string :as str]
             [fzf.core :refer [fzf]]))
 
 (defn- sh [& args]
@@ -11,13 +10,16 @@
 (defn- ->edn [s]
   (json/parse-string s true))
 
-(defn set-local [{:keys [g global]}]
-  (let [data (-> (sh "rtx ls --json") ->edn)
-        plugins (map name (keys data))
-        plugin (fzf plugins)
-        version (->> plugin keyword (get data) (map :version) fzf)]
-    (when (or (str/blank? plugin) (str/blank? version))
-      (throw (ex-info "couldn't parse plugin or version data" {:babashka/exit 1})))
-    (let [cmd (cond-> ["rtx" "use" (str plugin "@" version)]
-                (or g global) (conj "--global"))]
-      (apply sh cmd))))
+#_{:clj-kondo/ignore [:redefined-var]}
+(defn use [{:keys [g global]}]
+  (when-let [data (-> (sh "rtx ls --json") ->edn)]
+    (when-let [plugin (->> (keys data)
+                           (map name)
+                           (fzf))]
+      (when-let [version (->> (keyword plugin)
+                              (get data)
+                              (map :version)
+                              (fzf))]
+        (let [cmd (cond-> ["rtx" "use" (str plugin "@" version)]
+                    (or g global) (conj "--global"))]
+          (apply sh cmd))))))
