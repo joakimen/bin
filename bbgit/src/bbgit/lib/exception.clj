143595d302ff36d44b7d30a24406e3923a3ed32f
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-03-17 23:26:27 +0100

    replaced by joakimen/gi
    replaced by joakimen/gi

--- bbgit/src/bbgit/lib/exception.clj
+++
@@ -1,4 +0,0 @@
-(ns bbgit.lib.exception)
-
-(defn die [msg]
-  (throw (ex-info msg {:babashka/exit 1})))
--- bbgit/src/bbgit/lib/gum.clj
+++
@@ -1,5 +0,0 @@
-(ns bbgit.lib.gum
-  (:require [bblgum.core :as b]))
-
-(defn confirm []
-  (:result (b/gum :confirm :as :bool)))
