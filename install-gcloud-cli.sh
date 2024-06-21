1be0204029ef99ed3ed7825617f23325e7c76368
Joakim L. Engeset <joakim.engeset@gmail.com>
2024-06-21 14:30:40 +0200

    gcloud cli: add running the bundled install script

--- install-gcloud-cli.sh
+++ install-gcloud-cli.sh
@@ -2,7 +2,9 @@
 set -euo pipefail
 
 CLI_VERSION="481.0.0"
+SDK_DIR="google-cloud-sdk"
 arch=$(uname -sm)
+base_dir=$(pwd)
 
 function err_exit() {
   echo "Error: $1" >&2
@@ -16,8 +18,8 @@ Darwin\ arm64) filename_ident="darwin-arm.tar.gz" ;;
 *) err_exit "unsupported architecture: $arch" ;;
 esac
 
-filename="google-cloud-sdk-${CLI_VERSION}-${filename_ident}"
-url="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${filename}"
+tarball="google-cloud-sdk-${CLI_VERSION}-${filename_ident}"
+url="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/${tarball}"
 
 echo
 echo "cli_version: $CLI_VERSION"
@@ -26,17 +28,20 @@ echo "pwd: $(pwd)"
 echo "url: $url"
 echo
 
-echo "continue?"
-read -p "[y/N] " -r
+read -p "continue? [y/N] " -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   echo "exiting."
   exit 1
 fi
 
 echo
+echo "downloading cli version ${CLI_VERSION}..."
+curl -#SLO "$url"
 
 # check if google-cloud-sdk already exists
 if [ -d "google-cloud-sdk" ]; then
+
+  echo
 
   # ask user if they want to overwrite
   read -p "google-cloud-sdk directory already exists. do you want to overwrite it? [y/N] " -r
@@ -45,21 +50,35 @@ if [ -d "google-cloud-sdk" ]; then
     exit 1
   fi
   echo "removing old google-cloud-sdk directory..."
-  rm -rvf "google-cloud-sdk"
+  rm -rf "google-cloud-sdk"
 fi
-
-echo "downloading..."
-curl -#SLO "$url"
 
 echo
 echo "extracting..."
-tar -xzf "$filename"
+tar -xzf "$tarball"
 
 echo
+install_script="install.sh"
+install_flags="--quiet --usage-reporting=false --command-completion=true --path-update=true"
+echo "running google-cloud-sdk/install.sh..."
+
+echo "installing gcloud with the following flags: $install_flags"
+read -p "continue? [y/N] " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  echo "exiting."
+  exit 1
+fi
+
+cd "$SDK_DIR"
+# shellcheck disable=SC2086
+bash "$install_script" $install_flags
+
+echo "gcloud installation script completed."
 echo "cleaning up..."
-rm "$filename"
+rm "${base_dir}/$tarball"
 
 echo "done."
+
 echo
 echo "please ensure that the 'google-cloud-sdk/bin' directory is in your PATH:"
-echo "$(pwd)/google-cloud-sdk/bin"
+echo "$(pwd)/bin"
